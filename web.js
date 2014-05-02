9f45ae93a62fda6ea56a85069c66fe7b7864d6fe
Kevin-Jonaitis <kevitis117@gmail.com>
2014-05-02 02:08:57 -0700

    Added signup logic

--- web.js
+++ web.js
@@ -1,11 +1,29 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI ||  process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
+});
+
+app.post('/signUpCheck', function(req, res) {
+  var string = "You tried to sign up!"
+  string = string + " username: " + res.body.username + "<br> password: " + res.body.password; 
+  res.send(string);
+
+//  mongo.Db.connect(mongoUri, function (err, db) {
+//  db.collection('mydocs', function(er, collection) {
+//    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//    });
+//  });
+//});
 });
 
 app.use(express.static(__dirname + "/public"));
